312e0be11cffc4533298d8beb4061e7b0ce8df19
Alisha <alisha@gmail.com>
2026-09-22 13:35:36 +0530

    Milestone 2: Faculty module completed

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ console.log("College Management System loaded");
 function addStudent(name, roll) {
   console.log(`Student Added: ${name} - Roll No: ${roll}`);
 }
+
+function addFacultyMember(name, subject) {
+  console.log(`Faculty Added: ${name} - Subject: ${subject}`);
+}
